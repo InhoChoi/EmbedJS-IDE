3d64ba4ce52b1cd167612793f27961f0dc592176
inho <InhoChoi@inhoui-MacBook-Pro.local>
2015-10-26 18:38:57 +0900

    자동완성 기능 추가

--- EmbedJS IDE.app/Contents/Resources/default_app/js/editor.js
+++ EmbedJS IDE.app/Contents/Resources/default_app/js/editor.js
@@ -2,7 +2,9 @@
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/javascript");
-
+editor.setOptions({
+    enableBasicAutocompletion: true
+});
 
 //Editor의 내용이 변경되었을 경우  
 editor.getSession().on('change', function(e) {
