8525bc394c6adf094b0adc2b0586d995caede949
inho <InhoChoi@inhoui-MacBook-Pro.local>
2015-10-26 18:30:34 +0900

    editor js 설정 옮김

--- EmbedJS IDE.app/Contents/Resources/default_app/js/editor.js
+++ EmbedJS IDE.app/Contents/Resources/default_app/js/editor.js
@@ -2,3 +2,11 @@
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/javascript");
+
+
+//Editor의 내용이 변경되었을 경우  
+editor.getSession().on('change', function(e) {
+    if (document.title[0] != '*') {
+        document.title = '*' + document.title;
+    }
+});
